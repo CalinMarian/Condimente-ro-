c44c330890086eef29706a909b7ae7fce904f089
ghiran calin marian <calinmarianghiran@gmail.com>
2022-08-24 20:47:40 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Condimente__ro_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
